d032eb3d329592875eec6990e5e4a51620682f9a
Noodzakelijk-Online <154475954+Noodzakelijk-Online@users.noreply.github.com>
2026-05-27 23:31:46 +0200

    Add document handling package

---
+++ src/document_handling/__init__.py
@@ -0,0 +1,5 @@
+from src.document_handling.duplicate_detector import DuplicateDetector
+from src.document_handling.document_priority import DocumentPriorityResolver
+from src.document_handling.version_control import DocumentVersionControl
+
+__all__ = ["DuplicateDetector", "DocumentPriorityResolver", "DocumentVersionControl"]
